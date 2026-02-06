078d39d83915bb84667765e68f16793b35753caa
Mbongeni Mahlangu <Mbongenimahlangu203@outlook.com>
2026-02-06 15:45:28 +0200

    Work Done:
    - Rectified Model structure in relation to the revised DB model
    - Migrated updated models to database
    - Created Base DTOs for Modeical Options and Category
    - Populated database with policy contributions

--- Server/HRConnect.Api/DTOs/MedicalOption/MedicalOptionCategoryDto.cs
+++ Server/HRConnect.Api/DTOs/MedicalOption/MedicalOptionCategoryDto.cs
@@ -1,41 +1,39 @@
-﻿namespace HRConnect.Api.Models
+﻿namespace HRConnect.Api.DTOs.MedicalOption
 {
-  using System.ComponentModel.DataAnnotations;
-  using System.ComponentModel.DataAnnotations.Schema;
 
-  public class MedicalOptionCategory
+  using HRConnect.Api.Models;
+
+  public class MedicalOptionCategoryDto
   {
-    [Key]
-    [Column("MedicalOptionCategoryId")]
+    // Fecth all Medical Options grouped by Category, Incl Salary Bracket
+
     public int MedicalOptionCategoryId { get; set; }
-    [Required]
+
     public string MedicalOptionCategoryName { get; set; } = string.Empty;
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal? MonthlyRiskContributionPrincipal { get; set; }
-    [Required]
-    [Column(TypeName = "decimal(15, 2)")]
+
+
     public decimal MonthlyRiskContributionAdult { get; set; }
-    [Required]
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal MonthlyRiskContributionChild { get; set; }
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal? MonthlyRiskContributionChild2 { get; set; }
-    [Column(TypeName = "decimal(15, 2)")] 
+
     public decimal? MonthlyMsaContributionPrincipal { get; set; }
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal? MonthlyMsaContributionAdult { get; set; }
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal? MonthlyMsaContributionChild { get; set; }
-    //TODO: Add Total Monthly Contributions field - Done
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal? TotalMonthlyContributionsPrincipal { get; set; }
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal TotalMonthlyContributionsAdult { get; set; }
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal TotalMonthlyContributionsChild { get; set; }
-    [Column(TypeName = "decimal(15, 2)")]
+
     public decimal? TotalMonthlyContributionsChild2 { get; set; }
-    [Required]
+
     public MedicalOption MedicalOption { get; set; } = null!;
   }
 }
---
+++ Server/HRConnect.Api/Migrations/20260206094523_MedicalOptionsAndCategoryModelUpdateV1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRConnect.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MedicalOptionsAndCategoryModelUpdateV1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SalaryBracketMin",
+                table: "MedicalOptions",
+                type: "decimal(15,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(15,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SalaryBracketMax",
+                table: "MedicalOptions",
+                type: "decimal(15,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(15,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrincipalAmount",
+                table: "MedicalOptions",
+                type: "decimal(15,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(15,2)");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "AdultAmount",
+                table: "MedicalOptions",
+                type: "decimal(15,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdultAmount",
+                table: "MedicalOptions");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SalaryBracketMin",
+                table: "MedicalOptions",
+                type: "decimal(15,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(15,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SalaryBracketMax",
+                table: "MedicalOptions",
+                type: "decimal(15,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(15,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrincipalAmount",
+                table: "MedicalOptions",
+                type: "decimal(15,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(15,2)",
+                oldNullable: true);
+        }
+    }
+}
