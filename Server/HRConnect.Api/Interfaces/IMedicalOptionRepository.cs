41b615bc83b22290cb6a2d1dcfb2641582ae89dd
Mbongeni Mahlangu <Mbongenimahlangu203@outlook.com>
2026-02-11 16:02:21 +0200

    Work Done: - Refactor Interfaces for Repositories, Services and controller(used Grouped Inferface for easier grouping on service and repository). - Updated Controller get endpoint signature to match with the name used on the service and repository - Implemented method for fetching all options in their respective groups/categories on the following levels:   -=> Interface (Repository, Service)   -=> Repository (Interface method implemeted and expanded)   -=> Service (Interface method implemeted and expanded) - Implemented Mapper(Manual mapping) logic on the options mappers - Documented the methods on the Service, Repository and Mapper in detail
    Next task to do:
    - Revise and complete controller Get implementation
    - Test via Swagger UI
    - Structure the Put endpoint and the dependancies(Dto,Services,Repository, Interface methods) it will requires for a successful update on the database table
    
    Signed-off-by: Mbongeni Mahlangu <Mbongenimahlangu203@outlook.com>

--- Server/HRConnect.Api/Interfaces/IMedicalOptionRepository.cs
+++ Server/HRConnect.Api/Interfaces/IMedicalOptionRepository.cs
@@ -1,12 +1,20 @@
 ﻿namespace HRConnect.Api.Interfaces
 {
+  using HRConnect.Api.DTOs.MedicalOption;
   using HRConnect.Api.Models;
 
 
   public interface IMedicalOptionRepository
   {
     //TODO: Document methods
-
+    //1. GetGroupedMedicalOptions
+    //2. GetMedicalOptionById
+    //3. GetMedicalOptionByCategory
+    //4. GetMedicalOptionBySalaryBracket
+    //5. GetMedicalOptionByEmployee
+    //6. UpdateSalaryBracket (Put)
+    
+    Task<List<IGrouping<int, MedicalOption>>> GetGroupedMedicalOptionsAsync();
     
   }
 }
--- Server/HRConnect.Api/Interfaces/IMedicalOptionService.cs
+++ Server/HRConnect.Api/Interfaces/IMedicalOptionService.cs
@@ -1,8 +1,10 @@
-﻿namespace HRConnect.Api.Interfaces;
-
-
-
-public interface IMedicalOptionService
+﻿namespace HRConnect.Api.Interfaces
 {
+  using HRConnect.Api.DTOs.MedicalOption;
+  using HRConnect.Api.Models;
 
+  public interface IMedicalOptionService
+  {
+    Task<List<MedicalOptionCategoryDto>> GetGroupedMedicalOptionsAsync();
+  }  
 }
