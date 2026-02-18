11527462d4c22f54c9d0e82cc9334129a80e508e
Bandile Ndlela <BNdlela@SINGULAR.CO.ZA>
2026-02-18 09:49:41 +0200

    Fix Employmentemploymentstatus to EmploymentStatus in PensionProjectionTest.cs and pensionOptionTest

---
+++ Server/HRConnect.Tests/PensionOptionTests.cs
@@ -0,0 +1,30 @@
+﻿namespace HRConnect.Tests
+{
+  using HRConnect.Api.Utils;
+
+  public class PensionOptionTests
+  {
+    [Fact]
+    public void GetPensionOptionsReturnAFloat()
+    {
+      //Arrange
+      int index = 1;
+
+      //Act
+      float result = PensionOption.GetPensionPercentage(index);
+
+      //Assert
+      _ = Assert.IsType<float>(result);
+    }
+
+    [Fact]
+    public void GetPensionPercentageInvalidIndexThrowsException()
+    {
+      //Arrange
+      int invalidIndex = 0;
+
+      //Act & Assert
+      _ = Assert.Throws<IndexOutOfRangeException>(() => PensionOption.GetPensionPercentage(invalidIndex));
+    }
+  }
+}
