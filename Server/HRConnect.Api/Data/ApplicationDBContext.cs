e52bbfaf48e86d482b05df79e2cd653cee24180f
rebaseilane <rebaoneseilane@gmail.com>
2026-02-23 10:01:04 +0200

    FixSnapshotCorruption

--- Server/HRConnect.Api/Data/ApplicationDBContext.cs
+++ Server/HRConnect.Api/Data/ApplicationDBContext.cs
@@ -1,103 +1,68 @@
 namespace HRConnect.Api.Data
 {
-  using HRConnect.Api.Models;
-  using Microsoft.EntityFrameworkCore;
-  public class ApplicationDBContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
-  {
-    public DbSet<User> Users { get; set; }
-    public DbSet<Employee> Employees { get; set; }
-    public DbSet<Position> Positions { get; set; }
-    public DbSet<PasswordResetPin> PasswordResetPins { get; set; }
-    public DbSet<PasswordHistory> PasswordHistories { get; set; }
+    using HRConnect.Api.Models;
+    using Microsoft.EntityFrameworkCore;
 
-
-    // Model Configurations ----------------------------------------------------
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+        : DbContext(options)
     {
-      base.OnModelCreating(modelBuilder);
+        public DbSet<User> Users { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<PasswordResetPin> PasswordResetPins { get; set; }
+        public DbSet<PasswordHistory> PasswordHistories { get; set; }
 
-      modelBuilder.Entity<Employee>()
-        .HasOne(e => e.Position)
-        .WithMany(p => p.Employees)
-        .HasForeignKey(e => e.PositionId);
+        public DbSet<PayrollDeduction> PayrollDeductions { get; set; }
+        public DbSet<TaxTableUpload> TaxTableUploads { get; set; }
+        public DbSet<TaxDeduction> TaxDeductions { get; set; }
+        public DbSet<AuditPayrollDeductions> AuditPayrollDeductions { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-      modelBuilder.Entity<Employee>()
-        .HasOne(e => e.CareerManager)
-        .WithMany(e => e.Subordinates)
-        .HasForeignKey(e => e.CareerManagerID)
-        .OnDelete(DeleteBehavior.Restrict);
+            // Employee relationships
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Position)
+                .WithMany(p => p.Employees)
+                .HasForeignKey(e => e.PositionId);
 
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.CareerManager)
+                .WithMany(e => e.Subordinates)
+                .HasForeignKey(e => e.CareerManagerID)
+                .OnDelete(DeleteBehavior.Restrict);
 
-      // Convert enums to stirng in the database
+            // Enum conversions
+            modelBuilder.Entity<Employee>().Property(e => e.Title).HasConversion<string>();
+            modelBuilder.Entity<Employee>().Property(e => e.Gender).HasConversion<string>();
+            modelBuilder.Entity<Employee>().Property(e => e.Branch).HasConversion<string>();
+            modelBuilder.Entity<Employee>().Property(e => e.EmploymentStatus).HasConversion<string>();
 
-      modelBuilder.Entity<Employee>()
-        .Property(e => e.Title)
-        .HasConversion<string>();
+            // TaxDeduction
+            modelBuilder.Entity<TaxDeduction>(entity =>
+            {
+                entity.ToTable("TaxDeduction");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.TaxYear).IsRequired();
+                entity.Property(e => e.Remuneration).HasPrecision(12, 2).IsRequired();
+                entity.Property(e => e.AnnualEquivalent).HasPrecision(12, 2).IsRequired();
+                entity.Property(e => e.TaxUnder65).HasPrecision(12, 2).IsRequired();
+                entity.Property(e => e.Tax65To74).HasPrecision(12, 2).IsRequired();
+                entity.Property(e => e.TaxOver75).HasPrecision(12, 2).IsRequired();
+                entity.HasIndex(e => new { e.TaxYear, e.Remuneration }).IsUnique();
+            });
 
-      modelBuilder.Entity<Employee>()
-        .Property(e => e.Gender)
-        .HasConversion<string>();
-
-      modelBuilder.Entity<Employee>()
-        .Property(e => e.Branch)
-        .HasConversion<string>();
-
-      modelBuilder.Entity<Employee>()
-        .Property(e => e.EmploymentStatus)
-        .HasConversion<string>();
-
-
-
+            // TaxTableUpload
+            modelBuilder.Entity<TaxTableUpload>(entity =>
+            {
+                entity.ToTable("TaxTableUpload");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.TaxYear).IsRequired();
+                entity.Property(e => e.FileName).IsRequired();
+                entity.Property(e => e.FileUrl).IsRequired();
+                entity.Property(e => e.EffectiveFrom).IsRequired();
+            });
+        }
     }
-
-  }
-namespace HRConnect.Api.Data
-{
-  using System.Reflection.Metadata;
-  using HRConnect.Api.Models;
-  using Microsoft.EntityFrameworkCore;
-
-  public class ApplicationDBContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
-  {
-    public DbSet<User> Users { get; set; }
-    public DbSet<PasswordResetPin> PasswordResetPins { get; set; }
-    public DbSet<PasswordHistory> PasswordHistories { get; set; }
-    public DbSet<Employee> Employees { get; set; }
-    public DbSet<PayrollDeduction> PayrollDeductions { get; set; }
-    public DbSet<TaxTableUpload> TaxTableUploads { get; set; }
-    public DbSet<TaxDeduction> TaxDeductions { get; set; }
-    public DbSet<AuditPayrollDeductions> AuditPayrollDeductions { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-      base.OnModelCreating(modelBuilder);
-      modelBuilder.Entity<TaxDeduction>(entity =>
-      {
-        entity.ToTable("TaxDeduction");
-        entity.HasKey(entity => entity.Id);
-        entity.Property(entity => entity.TaxYear).IsRequired();
-        entity.Property(entity => entity.Remuneration).HasPrecision(12, 2).IsRequired();
-        entity.Property(entity => entity.AnnualEquivalent).HasPrecision(12, 2).IsRequired();
-        entity.Property(entity => entity.TaxUnder65).HasPrecision(12, 2).IsRequired();
-        entity.Property(entity => entity.Tax65To74).HasPrecision(12, 2).IsRequired();
-        entity.Property(entity => entity.TaxOver75).HasPrecision(12, 2).IsRequired();
-        entity.HasIndex(entity => new { entity.TaxYear, entity.Remuneration }).IsUnique();
-      });
-
-      modelBuilder.Entity<TaxTableUpload>(entity =>
-      {
-        entity.ToTable("TaxTableUpload");
-        entity.HasKey(e => e.Id);
-
-        entity.Property(e => e.TaxYear).IsRequired();
-        entity.Property(e => e.FileName).IsRequired();
-        entity.Property(e => e.FileUrl).IsRequired();
-        entity.Property(e => e.UploadedAt);
-        entity.Property(e => e.EffectiveFrom).IsRequired();
-        entity.Property(e => e.EffectiveTo);
-      });
-
-    }
-  }
 }
