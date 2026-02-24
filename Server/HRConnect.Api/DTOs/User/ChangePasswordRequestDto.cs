4972c8de3646ed72d632be9fa26ea016c95571be
mmabena <mmabena@singular.co.za>
2026-02-24 20:26:55 +0200

    Start feature: UserChangePassword

---
+++ Server/HRConnect.Api/DTOs/User/ChangePasswordRequestDto.cs
@@ -0,0 +1,9 @@
+namespace HRConnect.Api.DTOs.User
+{
+    public class ChangePasswordRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string CurrentPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
