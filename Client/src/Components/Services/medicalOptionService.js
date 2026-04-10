b7e0f9e786a9bf48b42a8ee1d6bf57907ffc7aa3
Mbongeni Mahlangu <mbongenimahlangu203@outlook.com>
2026-04-10 17:01:48 +0200

    Work Done:
    - Partial implementation of api endpoint within the service script for Medical Options
    
    Signed-off-by: Mbongeni Mahlangu <Mbongenimahlangu203@outlook.com>

---
+++ Client/src/Components/Services/medicalOptionService.js
@@ -0,0 +1,56 @@
+﻿import api from '../../api/api';
+
+const basePath = 'medical-options';
+
+// Get Options grouped by Category
+export const getAllOptionsGroupedByCategory = async () =>
+{
+  //wrap around a try-catch block
+  try{
+      return await api.get(`${basePath}/categories`);
+  } catch(error) {
+      throw error;
+  }
+};
+
+export const getAllMedicalOptionsCategories = async () =>
+{
+  try{
+      return await api.get(`${basePath}/categories/all`);
+  } catch (error) {
+      throw error;
+  }
+};
+
+export const getMedicalOptionsSnapshot = async () => {
+  try{
+      return await api.get(`${bashPath}/snapshot`);
+  } catch (error) {
+      throw error;
+  }
+};
+
+export const getCategoryById = async (id) => {
+  try{
+      return await api.get(`${basePath}/${id}/category`);
+  }  catch(error) {
+      throw error;
+  }
+};
+
+export const getMedicalOptionsbyCategoryId = async (id) => {
+  try{
+      return await api.get(`${basePath}/${id}/category/options`);
+  } catch (error)
+  {
+      throw error;
+  }
+};
+
+export const getMedicalOptionsSalaryBracketMatchingEmployeeSalary = async (salaryAmount) => {
+  try{
+      return await api.get(`${basepath}/options/${salaryAmount}/salary-brakcet`);
+  } catch (error) {
+      throw error;
+  }
+};
