300c7fb8b17ce02d9375ecb9ae20e1860045bcb0
Tlhalefo Mokhadi <TMokhadi@SINGULAR.CO.ZA>
2026-04-23 14:06:34 +0200

    fix!: payroll rollover dependency fix

--- Server/HRConnect.Api/Program.cs
+++ Server/HRConnect.Api/Program.cs
@@ -93,7 +93,7 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     {
-      options.UseSqlServer(builder.Configuration.GetConnectionString("DBeaverConnection")!);
+      options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")!);
       options.AddInterceptors(new AuditSaveChangesInterceptor());
     });
 
@@ -181,7 +181,7 @@ builder.Services.AddQuartz(q =>
   {
     store.UseSqlServer(options =>
         {
-          options.ConnectionString = builder.Configuration.GetConnectionString("DBeaverConnection")!;
+          options.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
           options.TablePrefix = "quartz.QRTZ_";
         });
     store.UseSerializer<Quartz.Simpl.SystemTextJsonObjectSerializer>();
--- Server/HRConnect.Api/Repository/CompanyContributionRepository.cs
+++ Server/HRConnect.Api/Repository/CompanyContributionRepository.cs
@@ -1,51 +1,56 @@
 namespace HRConnect.Api.Repository
 {
-    using HRConnect.Api.Data;
-    using HRConnect.Api.Interfaces;
-    using HRConnect.Api.Models.CompanyContributions;
-    using Microsoft.EntityFrameworkCore;
+  using HRConnect.Api.Data;
+  using HRConnect.Api.Interfaces;
+  using HRConnect.Api.Models.CompanyContributions;
+  using Microsoft.EntityFrameworkCore;
 
-    public class CompanyContributionRepository : ICompanyContributionRepository
+  public class CompanyContributionRepository : ICompanyContributionRepository
+  {
+    private readonly ApplicationDBContext _context;
+
+    public CompanyContributionRepository(ApplicationDBContext context)
     {
-        private readonly ApplicationDBContext _context;
-
-        public CompanyContributionRepository(ApplicationDBContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<List<CompanyContribution>> GetAllAsync()
-        {
-            return await _context.CompanyContributions.ToListAsync();
-        }
-
-        public async Task<CompanyContribution?> GetByIdAsync(int id)
-        {
-            return await _context.CompanyContributions.FindAsync(id);
-        }
-
-        public async Task<CompanyContribution?> CreateCompanyContributionAsync(CompanyContribution companyContributionModel)
-        {
-            await _context.CompanyContributions.AddAsync(companyContributionModel);
-            await _context.SaveChangesAsync();
-            return companyContributionModel;
-        }
-
-        public async Task<CompanyContribution?> UpdateCompanyContributionAsync(CompanyContribution companyContributionModel)
-        {
-            _context.CompanyContributions.Update(companyContributionModel);
-            await _context.SaveChangesAsync();
-            return companyContributionModel;
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var entity = await GetByIdAsync(id);
-            if (entity != null)
-            {
-                _context.CompanyContributions.Remove(entity);
-                await _context.SaveChangesAsync();
-            }
-        }
+      _context = context;
     }
+    public async Task<bool> FindAllocatedContribution(int payrollRunId)
+    {
+      return await _context.EmployeeCompanyContributions
+           .AnyAsync(e => e.PayrollRunId == payrollRunId);
+    }
+
+    public async Task<List<CompanyContribution>> GetAllAsync()
+    {
+      return await _context.CompanyContributions.ToListAsync();
+    }
+
+    public async Task<CompanyContribution?> GetByIdAsync(int id)
+    {
+      return await _context.CompanyContributions.FindAsync(id);
+    }
+
+    public async Task<CompanyContribution?> CreateCompanyContributionAsync(CompanyContribution companyContributionModel)
+    {
+      await _context.CompanyContributions.AddAsync(companyContributionModel);
+      await _context.SaveChangesAsync();
+      return companyContributionModel;
+    }
+
+    public async Task<CompanyContribution?> UpdateCompanyContributionAsync(CompanyContribution companyContributionModel)
+    {
+      _context.CompanyContributions.Update(companyContributionModel);
+      await _context.SaveChangesAsync();
+      return companyContributionModel;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+      var entity = await GetByIdAsync(id);
+      if (entity != null)
+      {
+        _context.CompanyContributions.Remove(entity);
+        await _context.SaveChangesAsync();
+      }
+    }
+  }
 }
