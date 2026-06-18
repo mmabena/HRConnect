dd07ba0f57bee86b9d36af02e0643c2b32309840
Ntsika <NtsikaLuvuyiso.Nkonki2@Gmail.com>
2026-06-18 15:52:15 +0200

    re-adjustedPensionFund.Js

--- Client/src/api/PensionFund.js
+++ Client/src/api/PensionFund.js
@@ -2,87 +2,44 @@ import axios from "axios";
 
 const API_BASE = "http://localhost:5147/api/Pension";
 
-// OPTIONS
-export const getPensionOptions = async () => {
-const token = localStorage.getItem("authToken");
-const response = await axios.get(`${API_BASE}/options`, {
-headers: { Authorization: `Bearer ${token}` }
+const api = axios.create({
+  baseURL: API_BASE,
 });
-return response.data;
-};
 
-export const createPensionOption = async (option) => {
-const token = localStorage.getItem("authToken");
-const response = await axios.post(
-`${API_BASE}/options`,
-{
-ContributionPercentage: option.contributionPercentage, 
-},
-{
-headers: {
-Authorization: `Bearer ${token}`,
-"Content-Type": "application/json",
-},
-}
-);
-  return response.data;
-};
-
-
-export const updatePensionOption = async (option) => {
-const token = localStorage.getItem("authToken");
-const response = await axios.put(
-`${API_BASE}/options`,   
-{
-pensionOptionId: option.pensionOptionId,
-contributionPercentage: option.contributionPercentage,
-},
-{
-headers: {
-Authorization: `Bearer ${token}`,
-"Content-Type": "application/json",
-},
-}
-);
-return response.data;
-};
-
-
-// FUNDS
-export const getPensionFunds = async () => {
+ 
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("authToken");
-  const response = await axios.get(`${API_BASE}/funds`, {
-  headers: { Authorization: `Bearer ${token}` }
-  });
-  return response.data;
-};
-
-export const addPensionFund = async (fund) => {
-const token = localStorage.getItem("authToken");
-const response = await axios.post(`${API_BASE}/funds`, fund, {
-headers: {
-Authorization: `Bearer ${token}`,
-"Content-Type": "application/json"
-}
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
-return response.data;
-};
 
-export const deletePensionFund = async (id) => {
-  const token = localStorage.getItem("authToken");
-  const response = await axios.delete(`${API_BASE}/funds/${id}`, {
-  headers: { Authorization: `Bearer ${token}` }
-  });
-  return response.data;
-};
+export const getPensionOptions = () =>
+  api.get("/options").then((res) => res.data);
 
-export const deleteAllPensionOptions = async () => {
-  const token = localStorage.getItem("authToken");
-  const response = await axios.delete(`${API_BASE}/options/delete-all`, {
-  headers: { Authorization: `Bearer ${token}` }
-  });
-  return response.data;
-};
+export const createPensionOption = (option) =>
+  api.post("/options", {
+    contributionPercentage: option.contributionPercentage,
+  }).then((res) => res.data);
+
+export const updatePensionOption = (option) =>
+  api.put("/options", {
+    pensionOptionId: option.pensionOptionId,
+    contributionPercentage: option.contributionPercentage,
+  }).then((res) => res.data);
+
+export const deleteAllPensionOptions = () =>
+  api.delete("/options/delete-all").then((res) => res.data);
+
+export const getPensionFunds = () =>
+  api.get("/funds").then((res) => res.data);
+
+export const addPensionFund = (fund) =>
+  api.post("/funds", fund).then((res) => res.data);
+
+export const deletePensionFund = (id) =>
+  api.delete(`/funds/${id}`).then((res) => res.data);
 
 
 
